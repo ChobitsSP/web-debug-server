1ed0612c34e1d439eb6b5dfce95760031560da6d
Chobits <chobits@live.cn>
2019-11-07 19:00:42 +0800

    1

--- config/config.default.ts
+++ config/config.default.ts
@@ -31,6 +31,14 @@ export default (appInfo: EggAppInfo) => {
     }
   };
 
+  config.security = {
+    csrf: {
+      enable: false
+      // ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
+    }
+    // domainWhiteList: []
+  };
+
   config.logger = {
     dir: path.join(appInfo.baseDir, "logs")
   };
